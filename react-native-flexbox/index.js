be85af577f704b11f213b7db263057729a4b5ad6
vns_lzrg <veneesejulialazaraga@gmail.com>
2026-02-17 20:57:24 +0800

    Done with align content section

--- react-native-flexbox/index.js
+++ react-native-flexbox/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 
-import AlignSelfLayout from './components/AlignSelf';
+import AlignContentLayout from './components/AlignContent';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(AlignSelfLayout);
+registerRootComponent(AlignContentLayout);
