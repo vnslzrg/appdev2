256da6c18bab81f6115242efc4b57978bd50d9b5
vns_lzrg <veneesejulialazaraga@gmail.com>
2026-04-17 17:23:45 +0800

    Done with passing parameters to routes section

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -10,26 +10,39 @@ function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button onPress={() => navigation.navigate('Details')}>
+      <Button
+      onPress={() => {
+        /* 1. Navigate to the Details route with params */
+        navigation.navigate('Details', {
+          itemId: 86,
+          otherParam: 'anything you want here',
+        });
+      }}
+      >
         Go to Details
       </Button>
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ route}) {
   const navigation = useNavigation();
 
+  const { itemId, otherParam } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-      <Button onPress={() => navigation.push('Details')}>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        onPress={
+          () =>
+            navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100),
+            })
+        }
+      >
         Go to Details... again
-      </Button>
-      <Button onPress={() => navigation.goBack()}>Go back</Button>
-      <Button onPress={() => navigation.popTo('Home')}>Go to Home</Button>
-      <Button onPress={() => navigation.popToTop()}>
-        Go back to first screen in stack
       </Button>
     </View>
   );
